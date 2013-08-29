6e0f22da2d8d0d954e223998e15aa76615c4880b
bobby <bobby@bobby-ThinkPad-T420.(none)>
2013-08-29 10:36:54 -0700

    First crack at df script

---
+++ diskcheck.py
@@ -0,0 +1,20 @@
+import subprocess
+
+df = subprocess.Popen(["df", "/dev/sda1"], stdout=subprocess.PIPE)
+
+output = df.communicate()[0]
+
+device, size, used, available, percent, mountpoint = \
+    output.split("\n")[1].split()
+
+
+print "The partition mounted on /dev/sda1 is %s full" % (percent)
+
+usage_int = int(percent.replace("%", ""))
+
+if usage_int > 20:
+    print "sending email to Ben informing him his Blog is about o explode"
+    #call sendgrid to email someone
+
+else:
+    print "disk still has plenty of room!"
