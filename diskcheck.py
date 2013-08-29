710b6fecd65085e6d4adddf520244978be84d9b3
bobby <bobby@bobby-ThinkPad-T420.(none)>
2013-08-29 13:29:01 -0700

    added some SMTP stuff - still WIP

--- diskcheck.py
+++ diskcheck.py
@@ -1,6 +1,7 @@
 #scrpit for monitoring disk usage on the blog machine
 
 import subprocess
+import smtplib
 
 df = subprocess.Popen(["df", "/dev/sda1"], stdout=subprocess.PIPE)
 
@@ -18,8 +19,26 @@ usage_int = int(percent.replace("%", ""))
 
 #funciton for taking aciton once threshold is reached - goal is to email someone if disk usage exceeds 80%
 if usage_int > 80:
+
+    sender = "help@readyforzero.com"
+    recievers = ["engineering@readyforzero.com"]
+
+    message = """From: From RFZ Blog <help@readyforzero.com>
+    To: Enineering <engineering@readyforzero.com
+    Subject: Blog Machine disk usage Warning
+
+    This email is a friendly heads up that the Disk on the Blog machine is nearly full. Tell Ben to stop wiriting so much!
+    """
+
     print "sending email to Ben informing him his Blog is about o explode"
-    #call sendgrid to email someone
+    try:
+        smtpObj = smtplib.SMTP("host")
+        smtpObj.sendmail(sender,receivers,message)
+        print "Successfully sent email"
+    except SMTPException:
+        print "Error: unable to send email"
 
 else:
     print "disk still has plenty of room!"
+
+
