9efd08c97fb9972b01e92c99aadad4e3ad8bead8
bobby <bobby@bobby-ThinkPad-T420.(none)>
2013-08-29 12:36:30 -0700

    added some comments

--- diskcheck.py
+++ diskcheck.py
@@ -1,3 +1,5 @@
+#scrpit for monitoring disk usage on the blog machine
+
 import subprocess
 
 df = subprocess.Popen(["df", "/dev/sda1"], stdout=subprocess.PIPE)
@@ -10,8 +12,11 @@ device, size, used, available, percent, mountpoint = \
 
 print "The partition mounted on /dev/sda1 is %s full" % (percent)
 
+#create variable that is an intiger for easier use in functions
 usage_int = int(percent.replace("%", ""))
 
+
+#funciton for taking aciton once threshold is reached - goal is to email someone if disk usage exceeds 80%
 if usage_int > 80:
     print "sending email to Ben informing him his Blog is about o explode"
     #call sendgrid to email someone
